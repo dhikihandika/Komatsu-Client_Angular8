182aa112fa7a7d51817fedfa1708a8affcec1e8d
dhikihandika <dhikihandika36@gmail.com>
2019-11-07 10:20:35 +0700

    first commit

---
+++ src/app/services/report-service/report.ts
@@ -0,0 +1,18 @@
+/*
+Aothuor : dhkihandika
+Date    : 22/10/2019
+*/
+
+// Define an interface with the required properties
+// this is structure your HttpClient request to declare the type of the response object
+export class Ireports {
+    id: number;
+    nama_op: string;
+    model_type: string;
+    model_urut: number;
+    result_image: Blob;
+    result_checkbox: number;
+    result_presentase: number;
+    createdAt: number;
+    updatedAt: number
+}
